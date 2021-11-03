e3577fb749095702d0ca662b72d95b366debc118
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-11-03 09:08:41 +0000

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sean Bain on 03/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
