d1cf9a3d17bb6e77be0b9f51642c05502037fce3
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-11-04 08:10:36 +0000

    Finished Detail View.

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -50,6 +50,11 @@ struct FruitDetailView: View {
                         
                         // NUTRIENTS
                         
+                        
+                        FruitNutrientsView(fruit: fruit)
+                        
+                        
+                        
                         // SUBHEADLINE
                         
                         Text("Learn more about \(fruit.title)".uppercased())
